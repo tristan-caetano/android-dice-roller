f8f15108bf882d56e33ce77eeff6f3a3780a7258
Tristan Caetano <tristancostacaetano@gmail.com>
2022-01-27 17:39:55 -0500

    Added ability to change theme, comments

--- app/src/main/java/com/example/cis433_assignment_1/MainActivity.java
+++ app/src/main/java/com/example/cis433_assignment_1/MainActivity.java
@@ -1,15 +1,26 @@
+/*
+Tristan Caetano
+CIS 433
+Homework 1
+
+I made an app that acts as a dice roller for Dungeons and Dragons.
+However, custom dice rolls may be done as well
+ */
+
 package com.example.cis433_assignment_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import android.os.Bundle;
 
+// Main Activity that contains the main DnD dice rolls
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,9 +28,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Declaring main text view that contains the dice roll values
         TextView mText = (TextView) findViewById(R.id.textView);
 
+        // Declaring buttons that appear on this menu
         Button custom = (Button) findViewById(R.id.customRoll);
+        Button theme = (Button) findViewById(R.id.themeButton);
+        Button d20 = (Button) findViewById(R.id.d20);
+        Button d12 = (Button) findViewById(R.id.d12);
+        Button d10 = (Button) findViewById(R.id.d10);
+        Button d8 = (Button) findViewById(R.id.d8);
+        Button d6 = (Button) findViewById(R.id.d6);
+        Button d4 = (Button) findViewById(R.id.d4);
+
+        // Custom button, when clicked, it sends you to the custom roll menu
         custom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button d20 = (Button) findViewById(R.id.d20);
+        // Performs a D20 roll, and displays in the text view
         d20.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button d12 = (Button) findViewById(R.id.d12);
+        // Performs a D12 roll, and displays in the text view
         d12.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button d10 = (Button) findViewById(R.id.d10);
+        // Performs a D10 roll, and displays in the text view
         d10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button d8 = (Button) findViewById(R.id.d8);
+        // Performs a D8 roll, and displays in the text view
         d8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button d6 = (Button) findViewById(R.id.d6);
+        // Performs a D6 roll, and displays in the text view
         d6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,18 +90,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button d4 = (Button) findViewById(R.id.d4);
+        // Performs a D4 roll, and displays in the text view
         d4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 rollDice(4, mText);
             }
         });
+
+        // Changes the button themes to black and white until the menu is changed
+        theme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeTheme("#000000", "#FFFFFF");
+            }
+        });
     }
 
-    public void rollDice(int diceVal, TextView mText){
+    // Method that takes in a dice roll ceiling value and displays it in the given text view
+    // Used for both menus
+    public static void rollDice(int diceVal, TextView mText){
         Random rand = new Random();
         int printVal = rand.nextInt(diceVal);
         mText.setText(Integer.toString(printVal + 1));
+    }
+
+    // Changes the button themes to black and white for this menu only
+    public void changeTheme(String pTheme, String sTheme){
+
+        Button d20 = (Button) findViewById(R.id.d20);
+        Button d12 = (Button) findViewById(R.id.d12);
+        Button d10 = (Button) findViewById(R.id.d10);
+        Button d8 = (Button) findViewById(R.id.d8);
+        Button d6 = (Button) findViewById(R.id.d6);
+        Button d4 = (Button) findViewById(R.id.d4);
+        Button custom = (Button) findViewById(R.id.customRoll);
+        Button theme = (Button) findViewById(R.id.themeButton);
+
+        d20.setTextColor(Color.parseColor(pTheme));
+        d20.setBackgroundColor(Color.parseColor(sTheme));
+        d12.setTextColor(Color.parseColor(pTheme));
+        d12.setBackgroundColor(Color.parseColor(sTheme));
+        d10.setTextColor(Color.parseColor(pTheme));
+        d10.setBackgroundColor(Color.parseColor(sTheme));
+        d8.setTextColor(Color.parseColor(pTheme));
+        d8.setBackgroundColor(Color.parseColor(sTheme));
+        d6.setTextColor(Color.parseColor(pTheme));
+        d6.setBackgroundColor(Color.parseColor(sTheme));
+        d4.setTextColor(Color.parseColor(pTheme));
+        d4.setBackgroundColor(Color.parseColor(sTheme));
+        custom.setTextColor(Color.parseColor(pTheme));
+        custom.setBackgroundColor(Color.parseColor(sTheme));
+        theme.setTextColor(Color.parseColor(pTheme));
+        theme.setBackgroundColor(Color.parseColor(sTheme));
+
     }
 }
--- app/src/main/java/com/example/cis433_assignment_1/insertVal.java
+++ app/src/main/java/com/example/cis433_assignment_1/insertVal.java
@@ -1,14 +1,25 @@
+/*
+Tristan Caetano
+CIS 433
+Homework 1
+
+I made an app that acts as a dice roller for Dungeons and Dragons.
+However, custom dice rolls may be done as well
+ */
+
 package com.example.cis433_assignment_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+// Secondary Activity that allows the user to do custom dice rolls
 public class insertVal extends AppCompatActivity {
 
     @Override
@@ -16,11 +27,18 @@ public class insertVal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_val);
 
-        EditText inVal = (EditText) findViewById(R.id.editTextNumber);
-        TextView m2Text = (TextView) findViewById(R.id.textView2);
-        MainActivity main = new MainActivity();
-
+        // Declaring buttons that appear on this menu
         Button back = (Button) findViewById(R.id.backButton);
+        Button roll = (Button) findViewById(R.id.submitButton);
+        Button theme = (Button) findViewById(R.id.themeButton2);
+
+        // Declaring number input text box
+        EditText inVal = (EditText) findViewById(R.id.editTextNumber);
+
+        // Declaring main text view that contains the dice roll values
+        TextView m2Text = (TextView) findViewById(R.id.textView2);
+
+        // Back button that will bring the user back to the main menu
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,12 +47,37 @@ public class insertVal extends AppCompatActivity {
             }
         });
 
-        Button roll = (Button) findViewById(R.id.submitButton);
+        // Button that will take the number in the input box as a roll ceiling value, and roll it
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                main.rollDice(Integer.valueOf(inVal.getText().toString()), m2Text);
+                MainActivity.rollDice(Integer.valueOf(inVal.getText().toString()), m2Text);
             }
         });
+
+        // Changes the button themes to black and white until the menu is changed
+        theme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeTheme("#000000", "#FFFFFF");
+            }
+        });
+
+    }
+
+    // Changes the button themes to black and white for this menu only
+    public void changeTheme(String pTheme, String sTheme){
+
+        Button submit = (Button) findViewById(R.id.submitButton);
+        Button back = (Button) findViewById(R.id.backButton);
+        Button theme = (Button) findViewById(R.id.themeButton2);
+
+        submit.setTextColor(Color.parseColor(pTheme));
+        submit.setBackgroundColor(Color.parseColor(sTheme));
+        back.setTextColor(Color.parseColor(pTheme));
+        back.setBackgroundColor(Color.parseColor(sTheme));
+        theme.setTextColor(Color.parseColor(pTheme));
+        theme.setBackgroundColor(Color.parseColor(sTheme));
+
     }
 }
